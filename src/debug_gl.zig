69c710830e578e6df26502f1666708f83373e3e0
Andrew Kelley <superjoe30@gmail.com>
2016-08-25 17:37:45 -0700

    update to latest zig

--- src/debug_gl.zig
+++ src/debug_gl.zig
@@ -1,9 +1,9 @@
 const c = @import("c.zig");
 
-pub const is_on = if (@compile_var("is_release")) c.GL_FALSE else c.GL_TRUE;
+pub const is_on = if (@compileVar("is_release")) c.GL_FALSE else c.GL_TRUE;
 
 pub fn assert_no_err() {
-    if (!@compile_var("is_release")) {
+    if (!@compileVar("is_release")) {
         const err = c.glGetError();
         if (err != c.GL_NO_ERROR) {
             c.fprintf(c.stderr, c"GL error: %d\n", err);
